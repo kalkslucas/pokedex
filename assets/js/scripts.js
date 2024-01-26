a65a2779259689649aef8087765e0da97600ef30
Lucas Kalks <lucaskalks00@gmail.com>
2024-01-26 17:22:50 -0300

    Realizando testes com várias requisições HTTP

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-/*Variáveis*/
+//Variáveis
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`//Link da API buscando pokemons até o limite definido
@@ -24,18 +24,19 @@ function convertPokemonToLi(pokemon) {
 fetch(url)//Função FETCH API
   .then((res) => res.json())//Resposta do Fetch API em cima da URL do Pokeapi
   .then((jsonBody) => jsonBody.results)//Conversão do responseBody para json
-  
   .then((pokemonJsonBody = []) => {//Lista de pokemon trazido pelo responseBody
-    //console.log(pokemonJsonBody);
-    
+    console.log(pokemonJsonBody);
     pokemonsList.innerHTML += pokemonJsonBody.map(convertPokemonToLi).join('')//Para imprimir a lista de pokemom em HTML, a função map pegará o jsonBody, mapeará o valor com base na função da formatação Li e fará a junção sem nenhum separador
-  
-    //const listItems = [] //Array com a lista de itens "Pokemon"
-    //for (let i = 0;i < pokemonJsonBody.length; i++){//Varredura do Body
-    //  const pokemon = pokemonJsonBody[i]
-    //  listItems.push(convertPokemonToLi(pokemon)) //Acrescenta cada pokemom retornado no array
-    //}
-    //pokemonsList.innerHTML += listItems//Criação dos elementos HTML sobre cada array dentro do body
   })
   .catch((err) => console.log(err))
   .finally(() => console.log("Requisição concluída!"))
+
+
+Promise.all([
+  fetch(`https://pokeapi.co/api/v2/pokemon/1`),
+  fetch(`https://pokeapi.co/api/v2/pokemon/2`),
+  fetch(`https://pokeapi.co/api/v2/pokemon/3`),
+  fetch(`https://pokeapi.co/api/v2/pokemon/4`)
+]).then((results) =>{
+  console.log(results);
+})
