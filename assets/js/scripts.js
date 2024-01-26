70ea8a432331e2fc5b4c715532627fb58a3072f1
Lucas Kalks <lucaskalks00@gmail.com>
2024-01-26 16:44:40 -0300

    Verbosidade do código
    Utilizado a função Map para execução do código, tornando-o mais limpo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`//Link da API buscando pokemons até o limite definido
-let pokemonsList = document.querySelector(".pokemons-list")
+let pokemonsList = document.querySelector("#pokemons-list")
 
 /*Função que cria o elemento LI dos pokemóns*/
 function convertPokemonToLi(pokemon) {
@@ -21,15 +21,21 @@ function convertPokemonToLi(pokemon) {
   `
 }
 
-fetch(url)
+fetch(url)//Função FETCH API
   .then((res) => res.json())//Resposta do Fetch API em cima da URL do Pokeapi
   .then((jsonBody) => jsonBody.results)//Conversão do responseBody para json
-  .then((pokemonList) => {//Lista de pokemon trazido pelo responseBody
-    console.log(pokemonList);
-    for (let i = 0;i < pokemonList.length; i++){//Varredura do Body
-      const pokemon = pokemonList[i]
-      pokemonsList.innerHTML += convertPokemonToLi(pokemon)//Criação dos elementos HTML sobre cada array dentro do body
-    }
+  
+  .then((pokemonJsonBody = []) => {//Lista de pokemon trazido pelo responseBody
+    //console.log(pokemonJsonBody);
+    
+    pokemonsList.innerHTML += pokemonJsonBody.map(convertPokemonToLi).join('')//Para imprimir a lista de pokemom em HTML, a função map pegará o jsonBody, mapeará o valor com base na função da formatação Li e fará a junção sem nenhum separador
+  
+    //const listItems = [] //Array com a lista de itens "Pokemon"
+    //for (let i = 0;i < pokemonJsonBody.length; i++){//Varredura do Body
+    //  const pokemon = pokemonJsonBody[i]
+    //  listItems.push(convertPokemonToLi(pokemon)) //Acrescenta cada pokemom retornado no array
+    //}
+    //pokemonsList.innerHTML += listItems//Criação dos elementos HTML sobre cada array dentro do body
   })
   .catch((err) => console.log(err))
   .finally(() => console.log("Requisição concluída!"))
