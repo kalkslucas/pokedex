1e644d50186911d6f70f8d0520f0b92a89db4def
Lucas Kalks <lucaskalks00@gmail.com>
2024-01-31 13:45:31 -0300

    Definindo cores para cada tipo de pokemón

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,24 @@ function convertPokemonToLi(pokemon) {
     <span class="name">${pokemon.name}</span>
     <div class="detail">
       <ol class="types">
-        ${pokemon.types.map((type) => `<li class="type-item">${type}</li>`).join("")}
+        ${pokemon.types
+          .map((type) => `<li class="type-item">${type}</li>`)
+          .join("")}
       </ol>
       <img src="${pokemon.sprite}" alt="${pokemon.name}">
     </div>
   </li>
-  `
+  `;
 }
 
-const pokemonsList = document.querySelector("#pokemons-list")
+const pokemonsList = document.querySelector("#pokemons-list");
 
-pokeApi.getPokemons().then((pokemonJsonBody = []) => {//Lista de pokemon trazido pelo responseBody
+pokeApi
+  .getPokemons()
+  .then((pokemonJsonBody = []) => {
+    //Lista de pokemon trazido pelo responseBody
     console.log(pokemonJsonBody);
-    pokemonsList.innerHTML += pokemonJsonBody.map(convertPokemonToLi).join('')//Para imprimir a lista de pokemom em HTML, a função map pegará o jsonBody, mapeará o valor com base na função da formatação Li e fará a junção sem nenhum separador
+    pokemonsList.innerHTML += pokemonJsonBody.map(convertPokemonToLi).join(""); //Para imprimir a lista de pokemom em HTML, a função map pegará o jsonBody, mapeará o valor com base na função da formatação Li e fará a junção sem nenhum separador
   })
   .catch((err) => console.log(err))
-  .finally(() => console.log("Requisição concluída!"))
+  .finally(() => console.log("Requisição concluída!"));
--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,34 +1,34 @@
-const pokeApi = {}
+const pokeApi = {};
 
-function convertPokeApiDetailToPokemon(pokeDetail){
-  const pokemon = new Pokemon()
-  pokemon.name = pokeDetail.name
-  pokemon.order = pokeDetail.order
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.name = pokeDetail.name;
+  pokemon.order = pokeDetail.order;
 
-  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-  const type = types[0] 
-  
-  pokemon.types = types
-  pokemon.mainType = type
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const type = types[0];
 
-  pokemon.sprite = pokeDetail.sprites.other.dream_world.front_default
+  pokemon.types = types;
+  pokemon.mainType = type;
 
-  return pokemon
+  pokemon.sprite = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
-          .then((response) => response.json())//Transformando a lista recebida para novas requisições para os detalhes de cada pokemon em JSON
-          .then(convertPokeApiDetailToPokemon)
-}
+    .then((response) => response.json()) //Transformando a lista recebida para novas requisições para os detalhes de cada pokemon em JSON
+    .then(convertPokeApiDetailToPokemon);
+};
 
 pokeApi.getPokemons = (offset = 0, limit = 15) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`//Link da API buscando pokemons até o limite definido
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`; //Link da API buscando pokemons até o limite definido
 
-  return fetch(url)//Função FETCH API
-  .then((res) => res.json())//Resposta do Fetch API em cima da URL do Pokeapi em Json
-  .then((jsonBody) => jsonBody.results)//Resultado da lista de pokemons em JSON 
-  .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))//Mapeamento da lista de pokemons por requisição, buscando detalhes dos pokemons 
-  .then((detailRequests) => Promise.all(detailRequests))//Requisitando todas as URLs dos pokemons da lista
-  .then((pokemonsDetails) => (pokemonsDetails))//Retorno de todos os detalhes dos pokemons via requisição
-}
+  return fetch(url) //Função FETCH API
+    .then((res) => res.json()) //Resposta do Fetch API em cima da URL do Pokeapi em Json
+    .then((jsonBody) => jsonBody.results) //Resultado da lista de pokemons em JSON
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //Mapeamento da lista de pokemons por requisição, buscando detalhes dos pokemons
+    .then((detailRequests) => Promise.all(detailRequests)) //Requisitando todas as URLs dos pokemons da lista
+    .then((pokemonsDetails) => pokemonsDetails); //Retorno de todos os detalhes dos pokemons via requisição
+};
